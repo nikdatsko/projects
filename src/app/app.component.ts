836c6073e793a61d6b0a2a26231465b12cb8f918
nikdatsko <skvollow@gmail.com>
2017-08-23 11:43:07 +0200

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 import {AppService} from './app.service';
 import {Project} from './project.model';
@@ -6,7 +6,8 @@ import {Project} from './project.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
 
@@ -16,7 +17,14 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log('ngOnInit');
     this.appService.getProjects().subscribe(data => this.projects = data);
+  }
+
+  onImgLoad(item: Project) {
+    item.loaded = true;
+  }
+
+  onImgError(item: Project) {
+    item.fail = true;
   }
 }
--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,3 +1,7 @@
 export class Project {
   title: string;
+  loaded?: boolean;
+  fail?: boolean;
+  light?: boolean;
+  full?: boolean;
 }
